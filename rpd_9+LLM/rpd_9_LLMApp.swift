503d7557aeb7e960a624edb82a40a5b0f3eb8f0d
yolo <cwatkins2005@gmail.com>
2025-11-18 14:00:02 -0600

    Initial Commit

---
+++ rpd_9+LLM/rpd_9_LLMApp.swift
@@ -0,0 +1,17 @@
+//
+//  rpd_9_LLMApp.swift
+//  rpd_9+LLM
+//
+//  Created by Chris on 11/18/25.
+//
+
+import SwiftUI
+
+@main
+struct rpd_9_LLMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
